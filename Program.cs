acf3f235a327f17dc1acf50d5dbae818b28c79bd
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-12-14 17:31:23 +0000

    update

--- Program.cs
+++ Program.cs
@@ -10,7 +10,28 @@ namespace ExceptionHandlingAssignment
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("What is your age?");
+                int userAge = int.Parse(Console.ReadLine());
+                
+                if (userAge <= 0)
+                {
+                    throw new Exception();
+                }
 
+                int now = int.Parse(DateTime.Now.ToString("yyyy"));
+                int dob = (now - userAge);
+                int dob2 = (dob - 1);
+
+                Console.WriteLine("You were born in " + dob2 + " or " + dob);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error! You entered 0 or a number below it. Please enter a whole number.");
+                Console.ReadLine();
+            }
         }
     }
 }
